6067094e5a763e0e16ab6e993e22f1f82c0a027d
justin rebbeck <jrebbeck2bsd220.org>
2023-12-05 09:57:34 -0600

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Justin Rebbeck on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
