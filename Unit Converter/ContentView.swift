b92ae659dcd05370bfd0ea18fd7a6849ab980ec3
justin rebbeck <jrebbeck2bsd220.org>
2023-12-15 09:44:28 -0600

    added first view

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = ""
+    @State var FinalVal = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Unit Converter")
+                    .font(.title)
+                    .bold()
+                Image("ruler")
+                    .resizable()
+                    .frame(width: 400, height: 150)
+                Text("Convert with feet and meters")
+                Spacer()
+                
+                    TextField("Enter feet", text: $value)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 105, height: 150)
+                        .padding()
+                Button("Calculate") {
+                    FinalVal = (Double(value) ?? 0.0) * Double(3.28)
+                }
+                Text(String(FinalVal))
+                    Spacer()
+            }
+            .padding()
+            .background(Color.blue)
         }
-        .padding()
     }
 }
 
@@ -24,3 +43,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
