5a36f432fbfc3933958a6076fd34c09c218c543d
justin rebbeck <jrebbeck2bsd220.org>
2023-12-20 09:48:30 -0600

    finished

--- Unit Converter/feet to meters.swift
+++ Unit Converter/feet to meters.swift
@@ -1,13 +1,13 @@
 //
-//  ContentView.swift
+//  feet to meters.swift
 //  Unit Converter
 //
-//  Created by Justin Rebbeck on 12/5/23.
+//  Created by Justin Rebbeck on 12/18/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct View2: View {
     @State private var value = ""
     @State var FinalVal = 0.0
     var body: some View {
@@ -19,25 +19,21 @@ struct ContentView: View {
                 Image("ruler")
                     .resizable()
                     .frame(width: 400, height: 150)
-                Text("Convert with meters and feet")
+                Text("Convert with feet and meters")
                 Spacer()
                 
-                    TextField("Enter meters", text: $value)
+                    TextField("Enter feet", text: $value)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 115, height: 150)
+                        .frame(width: 105, height: 150)
                         .padding()
                 Button() {
-                   FinalVal = (Double(value) ?? 0.0) * Double(3.281)
+                   FinalVal = (Double(value) ?? 0.0) / Double(3.281)
                 } label: {
                     Text("Calculate")
                 }
                 .buttonStyle(BlueButton())
                 Text(String(FinalVal))
                      Spacer()
-                NavigationLink("Feet to meters") {
-                    View2()
-                }
-                .colorInvert()
             }
             .padding()
             .background(Color.blue)
@@ -45,19 +41,9 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct View2_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
-}
-
-struct BlueButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color(red: 0, green: 0, blue: 0.5))
-            .foregroundStyle(.white)
-            .clipShape(Capsule())
+        View2()
     }
 }
 
